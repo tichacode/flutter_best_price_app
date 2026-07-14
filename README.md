c3e00c8265f3039f7d4bbbac624b9939f0d16674
Nonticha Limsiroratana <tichacode@gmail.com>
2026-07-14 23:06:40 +0700

    create new Flutter application

---
+++ README.md
@@ -0,0 +1,4 @@
+# best_price_app
+
+Framework : Flutter
+Description : application / web for calculate top 3 of best price for shopping!
---
+++ android/app/src/main/kotlin/com/example/best_price_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.best_price_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
